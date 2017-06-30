906663eb807a3da675e8108c421d674411dff7e0
hzchenshiwen <hzchenshiwen@corp.netease.com>
2017-06-30 10:33:00 +0800

    debug:测试错误

--- template/src/main.js
+++
@@ -1,7 +0,0 @@
-import App from './App'
-import router from './router'
-import { Vue } from './env'
-
-Vue.config.productionTip = false
-// 引入公共模块
-Vue.use(window.__common);
--- template/src/router/index.js
+++
@@ -1,8 +0,0 @@
-import { VueRouter } from '../env'
-import hello from '@/views/hello.vue'
-
-export default new VueRouter([
-	{
-        path: '<%= name %>', component: hello
-    }
-]);
